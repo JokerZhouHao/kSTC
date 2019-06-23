63945eedc84c6d5870529468537b7b4193e4e952
zhou <joker187245@163.com>
2019-06-23 18:33:22 +0800

    完善QueryParams

--- src/main/java/entity/QueryParams.java
+++ src/main/java/entity/QueryParams.java
@@ -1,46 +1,143 @@
 package entity;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import spatialindex.spatialindex.Point;
+import utility.Global;
+import utility.IOUtility;
 import utility.io.LuceneUtility;
 
 public class QueryParams {
+	
+	public int rtreeFanout = 50;
+	public double steepDegree = 0.1;
+	public double steepOppositeDegree = 0.9;
+	public int zorderWidth = 1000;
+	public int zorderHeight = 1000;
+	
+	public int numSample = 0;
+	public int type = 0;
+	
+	public int k = 0;
+	public int numWord = 0;
+	
 	public Point location = null;
-	public List<String> sWords = new ArrayList<>();
-	public int k = 1;
+	public int minpts = 0;
 	public double epsilon = 0;
 	public double xi = 0;
-	public int minpts = 0;
+	public List<String> sWords = new ArrayList<>();
+	
+	public int maxPidNeighborsBytes = 0;
+	
+	private final static String head = "rFanout steepD steepOD zw    zh    ns   " + 
+			"t  k  nw  mpts   eps       xi       maxPNeiByte   ";
+	private final static String formatStr = "%-8d%-7s%-8s%-6d%-6d%-5d" +
+			   "%-3d%-3d%-4d%-7d%-10s%-9s%-14d";
 	
 	public QueryParams() {}
-
-	public QueryParams(Point location, List<String> sWords, int k, double epsilonOrXi, int minpts) {
-		super();
-		this.location = location;
-		this.sWords = sWords;
-		this.k = k;
-		this.epsilon = epsilonOrXi;
-		this.xi = epsilonOrXi;
-		this.minpts = minpts;
-	}
-	
-	public QueryParams(Point location, String words, int numWord, int k, double epsilonOrXi, int minpts) throws Exception{
-		super();
-		this.location = location;
-		this.sWords = this.getWords(words, numWord);
-		this.k = k;
-		this.epsilon = epsilonOrXi;
-		this.xi = epsilonOrXi;
-		this.minpts = minpts;
-	}
 	
 	public QueryParams(double epsilonOrXi, int minpts) {
 		this.epsilon = epsilonOrXi;
 		this.xi = epsilonOrXi;
 		this.minpts = minpts;
+	}
+	
+	public QueryParams(int rtreeFanout, double steepDegree, int zorderWidth, int zorderHeight, int numSample, int type,
+			int k, int numWord, int minpts, double epsilon, double xi, int maxPidNeighborsBytes) {
+		super();
+		this.rtreeFanout = rtreeFanout;
+		this.steepDegree = steepDegree;
+		this.steepOppositeDegree = 1 - steepDegree;
+		this.zorderWidth = zorderWidth;
+		this.zorderHeight = zorderHeight;
+		this.numSample = numSample;
+		this.type = type;
+		this.k = k;
+		this.numWord = numWord;
+		this.minpts = minpts;
+		this.epsilon = epsilon;
+		this.xi = xi;
+		this.maxPidNeighborsBytes = maxPidNeighborsBytes;
+	}
+	
+	//	public QueryParams(Point location, List<String> sWords, int k, double epsilonOrXi, int minpts) {
+	//	super();
+	//	this.location = location;
+	//	this.sWords = sWords;
+	//	this.k = k;
+	//	this.epsilon = epsilonOrXi;
+	//	this.xi = epsilonOrXi;
+	//	this.minpts = minpts;
+	//}
+	//
+	//public QueryParams(Point location, String words, int numWord, int k, double epsilonOrXi, int minpts) throws Exception{
+	//	super();
+	//	this.location = location;
+	//	this.sWords = this.getWords(words, numWord);
+	//	this.k = k;
+	//	this.epsilon = epsilonOrXi;
+	//	this.xi = epsilonOrXi;
+	//	this.minpts = minpts;
+	//}
+	//
+	
+	public void setCoordAndSWords(Point location, List<String> sWords) {
+		this.location = location;
+		this.sWords = sWords;
+	}
+	
+	public static void displays(List<QueryParams> qps) {
+		System.out.println(head);
+		for(QueryParams qp : qps) {
+			System.out.println(String.format(formatStr, qp.rtreeFanout, qp.steepDegree,
+					qp.steepOppositeDegree, qp.zorderWidth, qp.zorderHeight, qp.numSample,
+					qp.type, qp.k, qp.numWord, qp.minpts,
+					qp.epsilon, qp.xi, qp.maxPidNeighborsBytes));
+		}
+	}
+	
+	public static void display(QueryParams qp) {
+		System.out.println(head);
+		System.out.println(String.format(formatStr, qp.rtreeFanout, qp.steepDegree,
+				qp.steepOppositeDegree, qp.zorderWidth, qp.zorderHeight, qp.numSample,
+				qp.type, qp.k, qp.numWord, qp.minpts,
+				qp.epsilon, qp.xi, qp.maxPidNeighborsBytes));
+	}
+	
+	public static String resPath(QueryParams qp) {
+		return qp.toString() + ".csv";
+	}
+	
+	public static List<QueryParams> load(String path) throws Exception {
+		List<QueryParams> qps = new ArrayList<>();
+		QueryParams qp = null;
+		BufferedReader br = IOUtility.getBR(path);
+		String line = null;
+		String arr[] = null;
+		while(null != (line = br.readLine())) {
+			if(line.startsWith("#") || line.trim().equals(""))	continue;
+			arr = line.split(Global.delimiterSpace);
+			qp = new QueryParams(Integer.parseInt(arr[0]), Double.parseDouble(arr[1]), Integer.parseInt(arr[2]), 
+					Integer.parseInt(arr[3]), Integer.parseInt(arr[4]), Integer.parseInt(arr[5]), 
+					Integer.parseInt(arr[6]), Integer.parseInt(arr[7]), Integer.parseInt(arr[8]), 
+					Double.parseDouble(arr[9]), Double.parseDouble(arr[10]),
+					Integer.parseInt(arr[11]));
+			qps.add(qp);
+		}
+		br.close();
+		if(qps.isEmpty())	return null;
+		else return qps;
+	}
+	
+	public static String generateTestSample(int rtreeFanout, double steepDegree, int zorderWidth, int zorderHeight, int numSample, int type,
+			int k, int numWord, int minpts, double epsilon, double xi, int maxPidNeighborsBytes) {
+		return String.format("%s %s %s %s %s %s %s %s %s %s %s %s ", rtreeFanout, steepDegree, zorderWidth,
+							zorderHeight, numSample, type, k, numWord, minpts, epsilon, xi, maxPidNeighborsBytes);
 	}
 	
 	public List<String> getWords(String str, int numWord) throws Exception{
@@ -60,8 +157,32 @@ public class QueryParams {
 	
 	@Override
 	public String toString() {
-		return "QueryParams [location=[" + String.valueOf(location.getCoord(0)) + ", " + String.valueOf(location.getCoord(1)) + "], sWords=" + sWords + ", k=" + k + ", epsilonOrXi=" + epsilon
-				+ ", minpts=" + minpts + "]";
+		return String.format("rFanout=%s.steepD=%s.steepOD=%s.zw=%s.zh=%s.ns=%s.t=%s.k=%s.nw=%s.mpts=%s.eps=%s.xi=%s.maxPNeiByte=%s",
+				rtreeFanout, steepDegree,
+				steepOppositeDegree, zorderWidth, zorderHeight, numSample,
+				type, k, numWord, minpts,
+				epsilon, xi, maxPidNeighborsBytes);
+	}
+	
+	
+	public static void main(String[] args) throws Exception{
+		List<QueryParams> qps = new ArrayList<>();
+		QueryParams qp = new QueryParams(100, 0.3, 20, 30, 200, -1, 5, 3, 20, 0.1, 0.01, 50000000);
+		qps.add(qp);
+		qps.add(qp);
+		QueryParams.displays(qps);
+		System.out.println();
+		QueryParams.display(qp);
+		System.out.println();
+		
+		String path = Global.inputPath + File.separator + "sample" + File.separator + "result" + File.separator + "qptest.txt";
+		qps = QueryParams.load(path);
+		QueryParams.displays(qps);
+		System.out.println();
+		
+		System.out.println(QueryParams.resPath(qp));
+		System.out.println(qp);
+		
 	}
 	
 }
---
+++ src/main/java/utility/IOUtility.java
@@ -0,0 +1,71 @@
+package utility;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.RandomAccessFile;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * 简化创建IO相关类
+ * @author Monica
+ *
+ */
+public class IOUtility {
+	public static BufferedReader getBR(String fp) throws Exception{
+		return new BufferedReader(new FileReader(fp));
+	}
+	
+	public static BufferedWriter getBW(String fp) throws Exception{
+		return new BufferedWriter(new FileWriter(fp));
+	}
+	
+	public static BufferedWriter getBW(String fp, Boolean appand) throws Exception{
+		return new BufferedWriter(new FileWriter(fp, appand));
+	}
+	
+	public static DataOutputStream getDos(String fp) throws Exception{
+		return new DataOutputStream(new BufferedOutputStream(new FileOutputStream(fp)));
+	}
+	
+	public static DataInputStream getDis(String fp) throws Exception{
+		return new DataInputStream(new BufferedInputStream(new FileInputStream(fp)));
+	}
+	
+	public static BufferedInputStream getBis(String fp) throws Exception {
+		return new BufferedInputStream(new FileInputStream(fp));
+	}
+	
+	public static DataOutputStream getDGZos(String fp) throws Exception{
+		return new DataOutputStream(new GZIPOutputStream(new BufferedOutputStream(new FileOutputStream(fp))));
+	}
+	
+	public static DataInputStream getDGZis(String fp) throws Exception{
+		return new DataInputStream(new GZIPInputStream(new BufferedInputStream(new FileInputStream(fp))));
+	}
+	
+	/**
+	 * 写第一行
+	 * @param path
+	 * @param line
+	 * @throws Exception
+	 */
+	public static void writeFirstLine(String path, String line) throws Exception {
+		RandomAccessFile raf = new RandomAccessFile(path, "rw");
+		raf.seek(0);
+		raf.write(line.getBytes());
+		raf.close();
+	}
+	
+	public static void main(String[] args) throws Exception {
+	}
+}
